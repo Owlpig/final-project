726e77cab1f7c2393cb8a12a7337ad0e0f825fae
tuna282 <gersil@kth.se>
2021-04-01 16:09:12 +0200

    started registration

---
+++ client/src/components/ProfilePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withAuth } from '@okta/okta-react';
+
+export default withAuth(
+  const ProfilePage = () => {
+    const [user, setUser] = useState({user: ''})
+
+    
+  }
+)
